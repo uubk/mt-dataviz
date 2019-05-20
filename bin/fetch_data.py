4cd9ee42739ba81a25210b4a855d261d522e6c5b
Maximilian Falkenstein <falkensm@ethz.ch>
2019-05-20 18:15:28 +0200

    Add code to fetch data

---
+++ bin/fetch_data.py
@@ -0,0 +1,49 @@
+import gitlab
+import argparse
+import json
+import tempfile
+import zipfile
+import filecmp
+import shutil
+
+parser = argparse.ArgumentParser(description='Fetch files from gitlab')
+parser.add_argument('--url', dest='url', required=True,
+                    help='GitLab root URL')
+parser.add_argument('--token', dest='token', required=True,
+                    help='GitLab token')
+parser.add_argument('--config', dest='config', required=True,
+                    help='Configures which files to fetch')
+
+args = parser.parse_args()
+with open(args.config, "r") as cfgFile:
+    config = json.loads(cfgFile.read())
+    gl = gitlab.Gitlab(args.url, private_token=args.token)
+    gl.auth()
+
+    for task in config:
+        projectId = task["project"]
+        project = gl.projects.get(projectId)
+        pipelines = project.pipelines.list(ref=task["branch"], status='success')
+        # Find last pipeline
+        pipelines = sorted(pipelines, reverse=True, key=lambda x: x.id)
+        print ("Newest pipeline for project " + project.name + " on branch " + task["branch"] + " is " + str(pipelines[0].id) + " for commit " + pipelines[0].sha)
+        pipeline = pipelines[0]
+        for job in pipeline.jobs.list():
+            if job.name == task["job"]:
+                with tempfile.TemporaryFile(mode="w+b") as file:
+                    job = project.jobs.get(job.id)
+                    job.artifacts(streamed=True, action=file.write)
+                    zip = zipfile.ZipFile(file)
+                    for file in zip.filelist:
+                        if file.filename.endswith(".json"):
+                            content = zip.read(file)
+                            with tempfile.NamedTemporaryFile(mode="w+b", delete=False) as file2:
+                                file2.write(content)
+                                file2.flush()
+                                file2.close()
+                                if not filecmp.cmp(file2.name, task["dest"]):
+                                    print ("File has changed!")
+                                    shutil.copyfile(file2.name, task["dest"])
+
+
+
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='dataviz',
       description='Data visualization script',
       author='Maximilian Falkenstein',
       author_email='maxf@njsm.de',
-      packages=['pandas==0.24.2', 'matplotlib==3.1.0', 'Jinja2==2.10.1'],
+      packages=['pandas==0.24.2', 'matplotlib==3.1.0', 'Jinja2==2.10.1', 'python-gitlab==1.8.0'],
       )
 
 # mpld3 is required from "git+https://github.com/javadba/mpld3@display_fix", you'll need to git install manually
