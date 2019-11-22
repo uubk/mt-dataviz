5fdd7d48f53ca947263c0d07abf02f2cc483efdf
Maximilian Falkenstein <falkensm@ethz.ch>
2019-11-22 17:10:17 +0100

    Formatting adjustments

--- bin/plot_histogram.py
+++ bin/plot_histogram.py
@@ -3,6 +3,7 @@ import argparse
 
 import numpy as np
 import seaborn as sns
+from matplotlib.ticker import MultipleLocator
 from matplotlib import pyplot as plt
 
 parser = argparse.ArgumentParser(description='Plot gcd data file')
@@ -42,18 +43,18 @@ with open(filenameB, 'r') as file:
 
 print("Data loaded")
 
-sns.set()
-sns.set_style("whitegrid")
-sns.set_context('paper', font_scale=3)
+sns.set(rc={"xtick.bottom" : True, "ytick.left" : True})
+sns.set_style("whitegrid", rc={"xtick.bottom" : True, "ytick.left" : True})
+sns.set_context('paper', font_scale=3, rc={"xtick.bottom" : True, "ytick.left" : True})
 fig, axes = plt.subplots(2, 1, sharey="all", sharex="none")
 
-bins = np.arange(30000, step=500)
+bins = np.arange(5000, step=100)
 bins = np.append(bins, [10000000])
 ax = sns.distplot(dataA, bins=bins, kde=False, color=(0.12, 0.57, 0.71), ax=axes[0])
 ax2 = sns.distplot(dataB, bins=bins, kde=False, color=(0.38, 0.79, 0.45), ax=axes[1])
 
-ax.set(xlim=(0, 30000))
-ax2.set(xlim=(0, 30000))
+ax.set(xlim=(0, 5000))
+ax2.set(xlim=(0, 5000))
 #ax2.set(xlim=(126, 128))
 #ax.spines['right'].set_visible(False)
 #ax2.spines['left'].set_visible(False)
@@ -66,6 +67,12 @@ ax.set_xlabel('time (us)')
 ax2.set_yscale('log')
 ax2.set_ylabel('Count\n(libint)')
 ax2.set_xlabel('time (us)')
+ax.xaxis.set_minor_locator(MultipleLocator(500))
+ax.tick_params(which='minor', length=8)
+ax.tick_params(which='major', length=6)
+ax2.xaxis.set_minor_locator(MultipleLocator(500))
+ax2.tick_params(which='minor', length=8)
+ax2.tick_params(which='major', length=6)
 
 fig.set_size_inches(18.5, 10)
 fig.tight_layout()
@@ -78,6 +85,6 @@ ax.set_xticklabels(labels)
 fig.canvas.draw()
 labels = [item.get_text() for item in ax2.get_xticklabels(which='both')]
 labels[-1] = "longer"
-ax2.set_xticklabels(labels)
+
 
 plt.savefig(output, bbox_inches='tight')
